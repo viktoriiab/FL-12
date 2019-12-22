b7b63199bb178a37629431481cf6ec33de06f171
Viktoriia Buts <viktoriiabuts@gmail.com>
2019-12-23 01:54:05 +0200

    HW6: input checkes updated

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -4,11 +4,14 @@ let a = prompt('Input first value "a" for a quadratic equation:');
 let b = prompt('Input second value "b" for a quadratic equation:');
 let c = prompt('Input third value "c" for a quadratic equation:');
 a = Number(a);
+b = Number(b);
+c = Number(c);
 if ( isNaN(a) || !isFinite(a) || Math.abs(a) === 0 ){
     correctInputes = false;
 }else{
     if ( isNaN(b) || !isFinite(b) ) {
         correctInputes = false;
+        console.log(typeof b);
     }else {
         if ( isNaN(c) || !isFinite(c) ){
             correctInputes = false;
