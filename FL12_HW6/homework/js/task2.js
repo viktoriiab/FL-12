ba1ab613fa3e4a20760d16b888c758fc3a28aef0
Viktoriia Buts <viktoriiabuts@gmail.com>
2019-12-23 22:35:49 +0200

    HW6: task2 - check for a positive definite length of sides

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -2,3 +2,17 @@ let a = prompt('Input first value of side "a" for a triangle:');
 let b = prompt('Input second value of side "b" for a triangle:');
 let c = prompt('Input third value of side "c" for a triangle:');
 
+if ( a.length < 1 || isNaN(a) || b.length < 1 || isNaN(b) || c.length < 1 || isNaN(c) ){
+    console.log('input values should be ONLY numbers!');
+}else{
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+
+    if( !((a ^ 0) === a && a > 0) || !((b ^ 0) === b && b > 0) || !((c ^ 0) === c && c > 0) ){
+        console.log('A triangle must have 3 sides with a positive definite length!');
+    }else{
+        console.log('ok');
+    }
+
+}
