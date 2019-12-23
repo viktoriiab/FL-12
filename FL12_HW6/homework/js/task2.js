26153e35311984fbf0f607b328b1aae172579680
Viktoriia Buts <viktoriiabuts@gmail.com>
2019-12-23 22:39:26 +0200

    HW6: task2 - add flag correctInputes

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,3 +1,4 @@
+let correctInputes = false;
 let a = prompt('Input first value of side "a" for a triangle:');
 let b = prompt('Input second value of side "b" for a triangle:');
 let c = prompt('Input third value of side "c" for a triangle:');
@@ -12,7 +13,6 @@ if ( a.length < 1 || isNaN(a) || b.length < 1 || isNaN(b) || c.length < 1 || isN
     if( !((a ^ 0) === a && a > 0) || !((b ^ 0) === b && b > 0) || !((c ^ 0) === c && c > 0) ){
         console.log('A triangle must have 3 sides with a positive definite length!');
     }else{
-        console.log('ok');
+        correctInputes = true;
     }
-
 }
