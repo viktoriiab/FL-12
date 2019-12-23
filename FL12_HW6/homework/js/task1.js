c7857c99f44df84fdf0ac7acd6bd49c5937dcc03
Viktoriia Buts <viktoriiabuts@gmail.com>
2019-12-23 20:44:23 +0200

    HW6: task1 - add solution of equation with discriminant

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,24 +1,47 @@
+const auxiliaryNumb2 = 2;
+const auxiliaryNumb4 = 4;
 let correctInputes = true;
+let x1 = 0, x2 = 0;
 
 let a = prompt('Input first value "a" for a quadratic equation:');
 let b = prompt('Input second value "b" for a quadratic equation:');
 let c = prompt('Input third value "c" for a quadratic equation:');
+
 a = Number(a);
 b = Number(b);
 c = Number(c);
+
 if ( isNaN(a) || !isFinite(a) || Math.abs(a) === 0 ){
     correctInputes = false;
 }else{
-    if ( isNaN(b) || !isFinite(b) ) {
+    if ( isNaN(b) || !isFinite(b)) {
         correctInputes = false;
-        console.log(typeof b);
     }else {
-        if ( isNaN(c) || !isFinite(c) ){
+        if ( isNaN(c) || !isFinite(c)){
             correctInputes = false;
         }
     }
 }
-    
-console.log(correctInputes);
 
+if(!correctInputes){
+    console.log('Invalid input data');
+}else{
+    let discriminant = Math.pow(b, auxiliaryNumb2) - auxiliaryNumb4 * a * c;
 
+    if ( discriminant >= 0 ){
+        if ( discriminant === 0 ){
+            if(b === 0) {
+                console.log('x = ', x1);
+            }else{
+                x1 = x2 = Math.round(- (b / (auxiliaryNumb2 * a) ) );
+                console.log('x = ', x1);
+            }
+        }else{
+            x1 = Math.round( (-b + Math.sqrt(discriminant) ) / (auxiliaryNumb2 * a) );
+            x2 = Math.round( (-b - Math.sqrt(discriminant) ) / (auxiliaryNumb2 * a) );
+            console.log('x1 = ',x1,'and x2 = ',x2);
+        }
+    }else{
+        console.log('No solution');
+    }
+}
