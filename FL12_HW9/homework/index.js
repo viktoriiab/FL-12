74627e42d380c0af2d6896c3665349051700ea66
Viktoriia Buts <viktoriiabuts@gmail.com>
2020-01-11 00:41:19 +0200

    HW9 : task4 filterArray function add

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -10,3 +10,37 @@ function convert(...theArgs){
     return convertedValues;
 }
 convert('1', 2, 3, '4');
+
+function executeforEach (arr, callback){
+    for ( let i = 0; i < arr.length; i++ ){
+      callback(arr[i]);
+    }
+}
+executeforEach([1,2,3], function(el) {console.log(el * 2)});
+
+function mapArray(arr, callback){
+    let transformedArray = [];
+    executeforEach(arr, function(el){
+        transformedArray.push(callback(el));
+    });
+    return transformedArray;
+}
+mapArray([2, '5', 8], function(el){
+    if( typeof el !== 'number' ){ 
+        return parseInt(el) + 3;
+    }
+    return el + 3;
+});
+
+function filterArray(arr,callback){
+    let transformedArray = [];
+    executeforEach(arr, function(el){
+        if(callback(el)){
+            transformedArray.push(el); 
+        }
+    });
+    return transformedArray; 
+}
+filterArray([2, 5, 8], function(el){
+    return el % 2 === 0; 
+});
