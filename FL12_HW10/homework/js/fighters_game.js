f6a7b65a7abdddd79451cb96d6a23fb2b04b8687
Viktoriia Buts <viktoriiabuts@gmail.com>
2020-01-18 20:28:27 +0200

    HW10 : function attack add

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,3 +1,4 @@
+const hundred = 100;
 class Fighter {
 
     constructor(prop) {
@@ -18,13 +19,40 @@ class Fighter {
     getHealth(){
         return this._prop.hp;
     }
+    attack(defender){
+        let result = false;
+        let success = Math.floor( Math.random() * hundred ) + 1;
+        let probability = hundred - ( defender.getStrength() + defender.getAgility() );
+        if ( success <= probability ){
+            result = true;
+        }
+        console.log(success);
+        console.log(probability);
+        console.log(result);
+        if( result ){
+            defender.dealDamage(this.getDamage());
+            console.log(`
+                ${this.getName()} make ${this.getDamage()} damage to ${defender.getName()}
+            `);
+        }else{
+            console.log(`
+                ${this.getName()} attack missed
+            `);
+        }
+    }
+    dealDamage(points){
+        let result = this.getHealth() - points;
+        if( result < 0 ){
+            this._prop.hp = 0;
+        }else{
+            this._prop.hp = result;
+        }
+    }
 }
 
 const fighter1 = new Fighter({name: 'Maximus', damage: 20, strength: 20, agility: 15, hp: 100});
 const fighter2 = new Fighter({name: 'Commodus', damage: 25, strength: 25, agility: 20, hp: 90});
-let name1 = fighter1.getName();
-console.log(name1); 
-let name2 = fighter2.getName();
-console.log(name2); 
-let hp1 = fighter1.hp;
-console.log(hp1); 
+
+
+fighter1.attack(fighter2);
+fighter2.attack(fighter1);
