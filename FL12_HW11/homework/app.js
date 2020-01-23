677e48dbd1c81906c133a672918c4e3769404ebf
Viktoriia Buts <viktoriiabuts@gmail.com>
2020-01-24 00:02:05 +0200

    HW11 : open/close functions added, formatted css and js code

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -39,16 +39,15 @@ const rootNode = document.getElementById('root');
 
 let state = {
   open : false,
-  visible : true
+  visible : false
 }
 function isEmpty(){
   const string = 'Folder is empty';
   let empty = document.createElement('span');
+  empty.classList.add('hidden');
   empty.innerHTML = string;
   return empty;
 }
-
-
 function isFolder(element){
   let _class = '';
   element['folder'] ? _class = 'folder' : _class = 'insert_drive_file';
@@ -64,104 +63,76 @@ function isVisible(element){
   element.visible ? _class += 'visible' : _class += 'hidden';
   return _class;
 }
+function opening(el){
+  while (el) {
+    if(el.nextSibling !== null){
+      if(el.nextSibling.classList.contains('hidden')){
+        el.nextSibling.classList.remove('hidden');
+        el.nextSibling.classList.add('visible');
+      }
+    }
+    el = el.nextSibling;
+  }
+}
+function closing(el){
+  while (el) {
+    if(el.nextSibling !== null){
+      if(el.nextSibling.classList.contains('visible')){
+        el.nextSibling.classList.remove('visible');
+        el.nextSibling.classList.add('hidden');
+      }
+    }
+    el = el.nextSibling;
+  }
+}
 function trigger() {
   document.addEventListener('click', function(event) {
-    let node = event.toElement.parentNode.classList;
-    console.log(node);
-    if( node.contains('folder') ){
-      if( node.contains('close') ){
-        event.target.firstElementChild.innerText = 'folder_open';
-        node.remove('close');
-        node.add('open');
-      }else{
-        event.target.firstElementChild.innerText = 'folder';
-        node.remove('open');
-        node.add('close');
+    if(event.toElement.parentNode.classList !== undefined 
+    && event.toElement.parentNode.classList !== null){
+      let node = event.toElement.parentNode.classList;
+      if( node.contains('folder') ){
+        if( node.contains('close') ){
+          event.target.firstElementChild.innerText = 'folder_open';
+          node.remove('close');
+          node.add('open');
+          opening(event.target);
+        }else{
+          if(event.target.firstElementChild !== null){
+            event.target.firstElementChild.innerText = 'folder';
+            node.remove('open');
+            node.add('close');
+            closing(event.target);
+          }
+        }
       }
     }
   });
 }
-
-
-
-function render(container,_obj){
-    
+function render(container,_obj){   
     _obj.forEach(el => {
       let node = document.createElement('div');
       let icon = document.createElement('i');
       let p = document.createElement('p');
       icon.innerHTML += `${isFolder(el)}`;
-      icon.classList.add('material-icons');
-      
+      icon.classList.add('material-icons');     
       p.appendChild(icon);
-      p.innerHTML += `${el['title']}`;
-      
-      
+      p.innerHTML += `${el['title']}`;        
       node.appendChild(p);
-      node.classList.add(isFolder(el), isOpen(el), isVisible(el));
+      node.classList.add(isFolder(el), isOpen(el), isVisible(state));
       container.appendChild(node);
-      
       if( !el['children'] ){
         if(el['children'] === null || el['children'] === false){
           node.appendChild(isEmpty());
         }
       }else{
+          if(node.parentNode.id === 'root'){
+            node.classList.remove('hidden');
+            node.classList.add('visible');
+          }
           render(node,el['children']);
       }
 
     });
-    /*let node = node.innerHTML += `
-    <div class = '${setClasses(_obj[i], state)}'>
-    <i class='material-icons'>${setClasses(_obj[i])}</i>
-    ${_obj[i]['title']}</div>
-    `;
-    console.log(node);*/
-
- /* for (let i = 0; i < _obj.length; i++){
-    let node = document.createElement('div');
-    container.innerHTML += `
-    <div class = '${setClasses(_obj[i], state)}'>
-    <i class='material-icons'>${setClasses(_obj[i])}</i>
-    ${_obj[i]['title']}</div>
-    `;
-    container.appendChild(node);
-
-    if( !_obj[i]['children'] ){
-      if(_obj[i]['children'] === null || _obj[i]['children'] === false){
-       container.appendChild(isEmpty());
-      }
-    }else{
-      render(container.firstElementChild,_obj[i]['children'], state);
-    }*/
-
-    /*console.log(container.children.length);
-    for(let j = 0; j < container.children.length; j++){
-
-    console.log(container.children[j]);
-    container.children[j].appendChild(isEmpty());
-    }
-      
-    }*/
-    
-    
-    
-
-    /*if( !_obj[i]['children'] ){
-      if(_obj[i]['children'] === null || _obj[i]['children'] === false){
-        node.appendChild(isEmpty());
-      }
-    }else{
-      if( node.parentNode.id === 'root'){
-        let defaultFolder = node.firstChild.nextSibling.classList;
-        defaultFolder.remove('hidden');
-        defaultFolder.add('visible');
-      }
-      render(node,_obj[i]['children'], state);
-    }*/
-      
-  
 }
-
 render(rootNode, structure);
 trigger();
-
